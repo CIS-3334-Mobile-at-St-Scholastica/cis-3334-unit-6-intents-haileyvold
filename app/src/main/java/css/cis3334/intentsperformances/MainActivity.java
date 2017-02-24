c56b2d0bd434d7678a0819108829c223d576bc41
Hailey Vold <hvold@css.edu>
2017-02-24 11:29:25 -0600

    In Class Commit

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -1,6 +1,7 @@
 package css.cis3334.intentsperformances;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -13,6 +14,8 @@ import java.util.GregorianCalendar;
 public class MainActivity extends AppCompatActivity {
 
     Button btn1Web, but1Calendar, btn1Map;
+    Button btn2Map, btn2Calendar, btn2Web;
+    Button btn3Web, btn3Calendar, btn3Map;
     TextView textViewStatus;
 
     @Override
@@ -40,6 +43,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn2Web = (Button) findViewById(R.id.button1WebInfo);
+        btn2Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display website for performance 2");
+            }
+        });
+
+        btn3Web = (Button) findViewById(R.id.button1WebInfo);
+        btn3Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display website for performance 3");
+            }
+        });
+
         /**
          *   Set up button click event listener for the celendar button for the first performance
          *       Use the calendar intent to set an event reminder on the calendar
@@ -64,6 +83,47 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        btn2Calendar = (Button) findViewById(R.id.button1Calendar);
+        btn2Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 2");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent);
+
+            }
+        });
+
+        btn3Calendar = (Button) findViewById(R.id.button1Calendar);
+        btn3Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 3");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startActivity(calIntent);
+
+            }
+        });
+
+
         /**
          *   Set up button click event listener for the web info button for the first performance
          */
@@ -71,7 +131,47 @@ public class MainActivity extends AppCompatActivity {
         btn1Map.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 // Do something in response to button click
-                textViewStatus.setText("Code should display map for performance 1");
+                textViewStatus.setText("Code should display map for performance 3");
+                Uri uri = Uri.parse("geo:46.8151347, -92.1044546");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn2Map = (Button) findViewById(R.id.button2Map);
+        btn2Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display map for performance 2");
+                Uri uri = Uri.parse("geo:46.8151347, -92.1044546");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
+            }
+        });
+
+        /**
+         *   Set up button click event listener for the web info button for the first performance
+         */
+        btn3Map = (Button) findViewById(R.id.button3Map);
+        btn3Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should display map for performance 3");
+                Uri uri = Uri.parse("geo:46.8151347, -92.1044546");
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+
+                if(intent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(intent);
+                }
             }
         });
 
